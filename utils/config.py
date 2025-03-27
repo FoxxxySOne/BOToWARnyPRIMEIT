0c680624a1e82a259a8edee7db57b5f41ebd3824
PRIMEIT <_email@example>
2025-03-27 13:52:47 +0300

    Автоматический коммит: Thu Mar 27 13:52:47 RTZ 2025

--- utils/config.py
+++
@@ -1,9 +0,0 @@
-# Размер страницы для пагинации
-PAGE_SIZE = 10
-
-# Путь к папкам
-DATA_FOLDER = "data"
-LOGS_FOLDER = "logs"
-
-# Типы файлов
-ALLOWED_FILE_EXTENSIONS = ['.xlsx', '.csv']
--- utils/error_loader.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-
-async def handle_error(update, error):
-    print(f"Ошибка: {error}")
-    if update and update.effective_message:
-        await update.effective_message.reply_text("❌ Произошла ошибка.")
